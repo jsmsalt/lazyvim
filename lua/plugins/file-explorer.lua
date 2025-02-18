a466ccd2c1ddfedf9df7d9571142a798a49eb74d
José Morales <jsmsalt@outlook.com>
2025-02-18 10:13:36 -0300

    feat: update config

--- lua/plugins/file-explorer.lua
+++ lua/plugins/file-explorer.lua
@@ -7,14 +7,23 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+      },
+      watch_for_changes = true,
+    },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
-    init = function()
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    end,
+    -- init = function()
+    --   vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    -- end,
   },
 }
